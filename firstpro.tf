0c2484d189794d7e01bf5e1df963f57edfdb8385
charankumar <kumar.charancp@gmail.com>
2025-11-25 07:10:33 +0000

    Added EC2 details OpenTofu script

---
+++ firstpro.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">= 1.7.0"
+
+  required_providers {
+    aws = {
+      source  = "registry.opentofu.org/hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+# Get all EC2 instance IDs in the region
+data "aws_instances" "all" {}
+
+# Fetch full details for each EC2 instance
+data "aws_instance" "details" {
+  for_each    = toset(data.aws_instances.all.ids)
+  instance_id = each.value
+}
+
+# Output EC2 details
+output "ec2_details" {
+  value = data.aws_instance.details
+}
